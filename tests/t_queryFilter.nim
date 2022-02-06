07e021d4ba2b706e0e8c06a981208262abc7b953
Nycto <James@RoundEights.com>
2022-02-06 13:47:54 -0800

    filterBoth and filterExcluding

--- tests/t_queryFilter.nim
+++ tests/t_queryFilter.nim
@@ -21,3 +21,20 @@ suite "QueryFilter":
         check(not filterBCD.evaluate({B, C}))
         check(not filterBCD.evaluate({C, D}))
         check(filterBCD.evaluate({B, C, D}))
+
+    test "Excluding values":
+        let filter = filterExcluding[TestEnum]({A, B})
+        check(filter.evaluate({C, D}))
+        check(filter.evaluate({}))
+        check(not filter.evaluate({A, B}))
+        check(not filter.evaluate({A}))
+        check(not filter.evaluate({B}))
+        check(not filter.evaluate({A, C, D}))
+        check(not filter.evaluate({B, C, D}))
+
+    test "Requiring both filters match":
+        let filter = filterBoth(filterMatching[TestEnum]({A, B}), filterExcluding[TestEnum]({C}))
+        check(filter.evaluate({A, B}))
+        check(filter.evaluate({A, B, D}))
+        check(not filter.evaluate({B, D}))
+        check(not filter.evaluate({A, B, C}))
