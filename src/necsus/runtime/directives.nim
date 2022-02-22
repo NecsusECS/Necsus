1c3c7620a11247d28045bad71c383bc827161bc4
Nycto <James@RoundEights.com>
2022-02-21 21:06:00 -0800

    Documentation improvement

--- src/necsus/runtime/directives.nim
+++ src/necsus/runtime/directives.nim
@@ -3,22 +3,26 @@ import entityId, options
 type
 
     Spawn*[C: tuple] = proc(components: sink C): EntityId
-        ## Describes a type that is able to create new entities
+        ## Describes a type that is able to create new entities. Where `C` is a tuple
+        ## with all the components to initially attach to this entity
 
     Delete* = proc(entityId: EntityId)
-        ## Deletes an entity
+        ## Deletes an entity and all associated components
 
     Attach*[C: tuple] = proc(entityId: EntityId, components: C)
-        ## Describes a type that is able to update existing entities new entities
+        ## Describes a type that is able to update existing entities new entities. Where `C` is
+        ## a tuple with all the components to attach.
 
     Detach*[C: tuple] = proc(entityId: EntityId)
-        ## Detaches a set of components from an entity
+        ## Detaches a set of components from an entity. Where `C` is a tuple describing all
+        ## the components to detach
 
     Lookup*[C: tuple] = proc(entityId: EntityId): Option[C]
-        ## Looks up entity details based on its entity ID
+        ## Looks up entity details based on its entity ID. Where `C` is a tuple with all the
+        ## components to fetch
 
     Outbox*[T] = proc(message: sink T): void
-        ## Sends an event
+        ## Sends an event. Where `T` is the message being sent
 
     TimeDelta* = float
         ## Tracks the amount of time since the last execution of a system
--- src/necsus/runtime/query.nim
+++ src/necsus/runtime/query.nim
@@ -1,20 +1,23 @@
 import entityId
 
 type
-    QueryItem*[T: tuple] = tuple[entityId: EntityId, components: T]
-        ## An individual value yielded by a query
+    QueryItem*[C: tuple] = tuple[entityId: EntityId, components: C]
+        ## An individual value yielded by a query. Where `C` is a tuple of the components to fetch in
+        ## this query
 
-    Query*[T: tuple] = proc(): iterator(): QueryItem[T]
-        ## Allows systems to query for entities with specific components
+    Query*[C: tuple] = proc(): iterator(): QueryItem[C]
+        ## Allows systems to query for entities with specific components. Where `C` is a tuple of
+        ## the components to fetch in this query.
 
-    Not*[T] = distinct int8
-        ## A query flag that indicates a component should be excluded from a query
+    Not*[C] = distinct int8
+        ## A query flag that indicates a component should be excluded from a query. Where `C` is
+        ## the single component that should be excluded.
 
-iterator pairs*[T: tuple](query: Query[T]): QueryItem[T] =
+iterator pairs*[C: tuple](query: Query[C]): QueryItem[C] =
     ## Iterates through the entities in a query
     let iter = query()
     for pair in iter(): yield pair
 
-iterator items*[T: tuple](query: Query[T]): T =
+iterator items*[C: tuple](query: Query[C]): C =
     ## Iterates through the entities in a query
     for (_, components) in query.pairs: yield components
