ec0da1b44333c0aebdf271962a4012b3597116cb
Nycto <James@RoundEights.com>
2022-04-11 08:43:55 -0700

    Add RingBuffer

---
+++ src/necsus/util/ringbuffer.nim
@@ -0,0 +1,145 @@
+##
+## Multi Producer/Multi Consumer Ring Buffer
+##
+## This is a lock free structure that allows multiple threads to push (produce) and pop (consume) data
+## in a FIFO way.
+##
+## The queue is bounded with a size of N where N must be a power of 2. The implementation however
+## allows to store only a maximum of N-1 elements.
+##
+## References:
+## * https://book-of-gehn.github.io/articles/2020/04/28/Lock-Free-Queue-Part-II.html
+## * https://github.com/eldipa/loki
+##
+
+import atomics, math, options
+
+type
+    RingBuffer*[N: static int, T] {.byref.} = object
+        ## Ring buffer instance
+
+        prodHead, prodTail: Atomic[uint]
+            ## On push (enqueue), the thread works as a producer:
+            ##  - it produces a new datum moving the head forward
+            ##  - and the datum enables the readers (consumers) to read it
+            ##    moving the tail forward to (yes, the push moves the tail too).
+
+        pad1: array[13, uint]
+            ## Pad between producer and consumer attributes. This avoids the "false sharing" problem: when we modify
+            ## and attribute, the whole L1/L2 cache line needs to be updated in all the cores. If at the same time other
+            ## thread is accessing to the other attributes a conflict will arise. The CPU will know how to fix it but it
+            ## is going to have a penalty.
+
+        consHead, consTail: Atomic[uint]
+            ##  On pop (dequeue), the thread works as a consumer:
+            ##   - it consumes a datum moving the tail forward
+            ##   - and moves the head forward too, let the writers (producers)
+            ##     know that there is a new free slot there.
+
+        pad2: array[13, uint]
+            ## More padding to prevent false sharing
+
+        data: array[N, T]
+
+template check(N: static int) =
+    static:
+        assert(N > 0)
+        assert(isPowerOfTwo(int(N)))
+
+template mask(N: static int): uint =
+    ## Assuming a size power of 2 N, we can compute X % N as X & mask for any integer. (where & is faster than %).
+    static:
+        assert(isPowerOfTwo(N))
+    N - 1
+
+proc `$`*[N: static int, T](ring: var RingBuffer[N, T]): string =
+    result.add("[")
+    var isFirst = true
+    for i in ring.consHead.load..<ring.prodHead.load:
+        if isFirst:
+            isFirst = false
+        else:
+            result.add(", ")
+        result.add($ring.data[i and mask(N)])
+    result.add("]")
+
+proc capacity*[N: static int, T](ring: RingBuffer[N, T]): uint =
+    ## Return the capacity of this buffer.
+    # We allocated a queue of size N, and by definition the mask is N-1.  # Now, the queue always leaves 1
+    # slot empty between the head  and the tail to differentiate a full queue from an empty queue
+    # so the capacity is also N-1
+    N.uint - 1
+
+proc tryPush*[N: static int, T](ring: var RingBuffer[N, T], value: sink T): bool =
+    ## Pushes a value onto the ring, returning true if it was a success
+    check(N)
+
+    while true:
+        block retry:
+            var oldProdHead = ring.prodHead.load(moRelaxed)
+
+            fence(moAcquire)
+
+            ## Step 1: Make sure there is capacity to store this value
+            let available = ring.capacity + ring.consTail.load(moAcquire) - oldProdHead
+            if available <= 0:
+                return false
+
+            # Step 2: Reserve a slot where we can store the new value
+            let newProdHead = oldProdHead + 1
+            if not ring.prodHead.compareExchange(oldProdHead, newProdHead, moRelaxed, moRelaxed):
+                break retry
+
+            # Step 3: Slot reserved, store the value
+            let idx = oldProdHead and mask(N)
+            ring.data[idx] = value
+
+            # Step 4: Update the head to announce the new value
+            while not ring.prodTail.compareExchange(oldProdHead, newProdHead, moRelease):
+                discard
+
+            return true
+
+proc tryShift*[N: static int, T](ring: var RingBuffer[N, T]): Option[T] =
+    ## Shifts a value from the ring
+    check(N)
+
+    block done:
+        while true:
+            block retry:
+                var oldConsHead = ring.consHead.load(moRelaxed)
+
+                fence(moAcquire)
+
+                # Step 1: Make sure there is something to pop
+                let available = ring.prodTail.load(moAcquire) - oldConsHead
+                if available <= 0:
+                    return none(T)
+
+                # Step 2: Request a slot to return
+                let newConsHead = old_cons_head + 1
+                if not ring.consHead.compareExchange(oldConsHead, newConsHead, moRelaxed, moRelaxed):
+                    break retry
+
+                # Step 3: Copy over the resulting data
+                let idx = oldConsHead and mask(N)
+                result = some(ring.data[idx])
+
+                # Step 4: Update the tail to announce the removed value
+                while not ring.consTail.compareExchange(oldConsHead, newConsHead, moRelease):
+                    discard
+
+                break done
+
+proc drain*[N: static int, T](ring: var RingBuffer[N, T]): seq[T] =
+    ## Removes all values from this buffer and puts them in a sequence
+    while true:
+        let value = ring.tryShift()
+        if value.isSome:
+            result.add(value.get)
+        else:
+            break
+
+proc isEmpty*[N: static int, T](ring: var RingBuffer[N, T]): bool =
+    ## Return whether this ring buffer is void of any values
+    (ring.prodHead.load - ring.consHead.load) == 0
---
+++ tests/t_ringbuffer.nim
@@ -0,0 +1,126 @@
+import unittest, necsus/util/ringbuffer, options, threadpool, std/sharedlist, sequtils, algorithm, random, atomics
+
+suite "RingBuffer":
+
+    test "tryPush and tryShift":
+        var q: RingBuffer[8, int]
+
+        check(q.tryPush(123))
+        check(q.tryPush(456))
+        check(q.tryPush(789))
+
+        check(q.tryShift() == some(123))
+        check(q.tryShift() == some(456))
+        check(q.tryShift() == some(789))
+
+    test "Draining":
+        var q: RingBuffer[8, int]
+        check(q.tryPush(123))
+        check(q.tryPush(456))
+        check(q.tryPush(789))
+        check(q.drain() == @[ 123, 456, 789 ])
+
+    test "Pushing onto a full queue":
+        var q: RingBuffer[4, int]
+        check(q.capacity == 3)
+
+        check(q.tryPush(1))
+        check(q.tryPush(2))
+        check(q.tryPush(3))
+        check(not q.tryPush(4))
+        check(not q.tryPush(5))
+
+    test "Wrapping around":
+        var q: RingBuffer[8, int]
+
+        for i in 0..<7:
+            check(q.tryPush(i))
+
+        for i in 7..<100:
+            require(q.tryShift() == some(i - 7))
+            require(q.tryPush(i))
+
+    test "Popping from an empty ring":
+        var q: RingBuffer[8, int]
+        check(q.tryShift().isNone)
+
+    test "Stringify":
+        var q: RingBuffer[8, int]
+        check($q == "[]")
+
+        check q.tryPush(123)
+        check($q == "[123]")
+
+        check q.tryPush(456)
+        check($q == "[123, 456]")
+
+        check q.tryPush(789)
+        check($q == "[123, 456, 789]")
+
+        discard q.tryShift
+        check($q == "[456, 789]")
+
+        discard q.tryShift
+        check($q == "[789]")
+
+        discard q.tryShift
+        check($q == "[]")
+
+    test "isEmpty":
+        var q: RingBuffer[8, int]
+        check(q.isEmpty)
+
+        discard q.tryPush(123)
+        check(not q.isEmpty)
+
+    test "Assert compile errors when size is wrong":
+        var q: RingBuffer[200, int]
+
+        when compiles(q.tryPush(123)):
+            check(false)
+
+        when compiles(q.tryShift()):
+            check(false)
+
+    test "Multi-threaded push and shift":
+        var q: RingBuffer[2048, int]
+
+        proc pushIt(value: int) {.gcsafe.} =
+            require(q.tryPush(value))
+
+        for i in 0..<2000:
+            spawn pushIt(i)
+
+        var results: SharedList[int]
+        results.init
+
+        proc shiftIt() =
+            results.add(q.tryShift().get)
+
+        for i in 0..<2000:
+            spawn shiftIt()
+
+        sync()
+
+        check(results.toSeq.sorted == toSeq(0..<2000))
+
+    for i in 0..10:
+        test "Random push and pop #" & $i:
+            var q: RingBuffer[2048, uint64]
+            var length: Atomic[int]
+
+            proc act(randomInt: uint64) {.gcsafe.} =
+                if randomInt mod 2 == 0:
+                    if q.tryPush(randomInt):
+                        length += 1
+                else:
+                    if q.tryShift().isSome:
+                        length -= 1
+
+            var rand = initRand(i)
+            for _ in 0..<2000:
+                spawn act(rand.next())
+
+            sync()
+
+            check(q.drain.len == length.load)
