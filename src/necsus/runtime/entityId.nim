32d113ff9bba5c26704f502afc5acb0817a8b318
Nycto <James@RoundEights.com>
2022-04-11 20:14:15 -0700

    Add equals and hash to EntityId

--- src/necsus/runtime/entityId.nim
+++ src/necsus/runtime/entityId.nim
@@ -1,3 +1,5 @@
+import hashes
+
 type
     EntityId* = distinct int32
         ## Identity of an entity
@@ -5,3 +7,10 @@ type
 proc `$`*(entityId: EntityId): string =
     ## Stringify an EntityId
     "EntityId(" & $int(entityId) & ")"
+
+proc `==`*(a, b: EntityId): bool =
+    ## Compare two entities
+    a.int32 == b.int32
+
+proc hash*(entityId: EntityId): Hash =
+    Hash(entityId.uint * 7)
