00c49bcbe3d11d71327149f8cbc446d403999cf0
Nycto <James@RoundEights.com>
2023-11-11 12:47:10 -0800

    Only add lookup else case if needed

--- src/necsus/compiletime/lookupGen.nim
+++ src/necsus/compiletime/lookupGen.nim
@@ -41,16 +41,26 @@ proc generateTuple(details: GenerateContext, arg: SystemArg, name: string, looku
 
         let procName = ident(name)
         let tupleType = lookup.args.toSeq.asTupleType
-
-        # Create a case statement where each branch is one of the archetypes
-        var cases = nnkCaseStmt.newTree(newDotExpr(entityIndex, ident("archetype")))
-        for (ofBranch, archetype) in archetypeCases(details):
-            if lookup.canCreateFrom(archetype):
-                cases.add(nnkOfBranch.newTree(ofBranch, details.buildArchetypeLookup(lookup, archetype)))
-
-        # Add a fall through 'else' branch for any archetypes that don't fit this lookup
         let noneResult = quote: return none(`tupleType`)
-        cases.add(nnkElse.newTree(noneResult))
+
+        var cases: NimNode
+        if details.archetypes.len == 0:
+            cases = noneResult
+
+        else:
+            cases = nnkCaseStmt.newTree(newDotExpr(entityIndex, ident("archetype")))
+
+            # Create a case statement where each branch is one of the archetypes
+            var needsElse = false
+            for (ofBranch, archetype) in archetypeCases(details):
+                if lookup.canCreateFrom(archetype):
+                    cases.add(nnkOfBranch.newTree(ofBranch, details.buildArchetypeLookup(lookup, archetype)))
+                else:
+                    needsElse = true
+
+            # Add a fall through 'else' branch for any archetypes that don't fit this lookup
+            if needsElse:
+                cases.add(nnkElse.newTree(noneResult))
 
         return quote:
             `appStateIdent`.`procName` = proc(`entityId`: EntityId): Option[`tupleType`] =
---
+++ tests/t_lookupWithoutArchetypes.nim
@@ -0,0 +1,16 @@
+import unittest, necsus, options, sequtils
+
+type
+    A = object
+    B = object
+    C = object
+
+proc doLookup(lookup: Lookup[(A, B, C)]) =
+    discard
+
+proc runner(tick: proc(): void) = tick()
+
+proc testLookup() {.necsus(runner, [], [~doLookup], [], newNecsusConf()).}
+
+test "Lookups without any archetypes in the system":
+    testLookup()
