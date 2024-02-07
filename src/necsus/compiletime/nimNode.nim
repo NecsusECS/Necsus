885a3e7098a72d4c0415e9effb688f93b06f69be
Nycto <James@RoundEights.com>
2024-02-06 18:51:53 -0800

    Allow refs in sys variables

--- src/necsus/compiletime/nimNode.nim
+++ src/necsus/compiletime/nimNode.nim
@@ -9,6 +9,7 @@ proc symbols*(node: NimNode): seq[string] =
     of nnkNilLit: return @["nil"]
     of nnkBracketExpr, nnkTupleTy: return node.toSeq.mapIt(it.symbols).foldl(concat(a, b))
     of nnkIdentDefs: return concat(node[0].symbols, node[1].symbols)
+    of nnkRefTy: return concat(@["ref"], node[0].symbols)
     else: error(&"Unable to generate a component symbol from node ({node.kind}): {node.repr}")
 
 proc hash*(node: NimNode): Hash =
@@ -19,6 +20,7 @@ proc hash*(node: NimNode): Hash =
     of nnkFloatLit..nnkFloat64Lit: return hash(node.floatVal)
     of nnkNilLit, nnkEmpty: return hash(0)
     of nnkBracketExpr, nnkTupleTy, nnkIdentDefs: return node.toSeq.mapIt(hash(it)).foldl(a !& b, hash(node.kind))
+    of nnkRefTy: return hash(node[0])
     else: error(&"Unable to generate a hash from node ({node.kind}): {node.repr}")
 
 proc cmp*(a: NimNode, b: NimNode): int =
