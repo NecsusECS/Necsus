a12f5342baf97ce73b7af341677b0dff9345fc72
Nycto <James@RoundEights.com>
2024-04-05 18:43:09 -0700

    Add additional flags to build workflow

--- buildall.sh
+++ buildall.sh
@@ -13,8 +13,10 @@ for nimVersion in 2.0.0 1.6.14; do
         act -W .github/workflows/build.yml -j example-projects --matrix "nim:$nimVersion" --matrix "project:$project";
     done
 
-    for target in readme profile float32; do
-        act -W .github/workflows/build.yml -j "$target" --matrix "nim:$nimVersion";
+    act -W .github/workflows/build.yml -j readme --matrix "nim:$nimVersion";
+
+    for flag in profile dump archetypes necsusFloat32; do
+        act -W .github/workflows/build.yml -j flags --matrix "nim:$nimVersion" --matrix "flag:$flag";
     done
 done
 
