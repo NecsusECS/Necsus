f4cd6488481ea9fbf11ba0e44a1aaf2f0017331c
Nycto <James@RoundEights.com>
2024-06-18 19:07:22 -0700

    Elide converter for fast builds

--- src/necsus/compiletime/converters.nim
+++ src/necsus/compiletime/converters.nim
@@ -5,28 +5,31 @@ import ../runtime/query
 let input {.compileTime.} = ident("input")
 let output {.compileTime.} = ident("output")
 
-proc read(fromVar: NimNode, fromArch: Archetype[ComponentDef], arg: DirectiveArg): NimNode =
+proc read(fromVar: NimNode, fromArch: Archetype[ComponentDef], arg: DirectiveArg): NimNode {.used.} =
     let readExpr = nnkBracketExpr.newTree(fromVar, newLit(fromArch.indexOf(arg.component)))
     return if arg.isPointer: nnkAddr.newTree(readExpr) else: readExpr
 
 proc copyTuple(fromVar: NimNode, fromArch: Archetype[ComponentDef], directive: TupleDirective): NimNode =
     ## Generates code for copying from one tuple to another
-    result = newStmtList()
-    for i, arg in directive.args:
-        let value = case arg.kind
-            of DirectiveArgKind.Exclude:
-                newCall(nnkBracketExpr.newTree(bindSym("Not"), arg.type), newLit(0'i8))
-            of DirectiveArgKind.Include:
-                fromVar.read(fromArch, arg)
-            of DirectiveArgKind.Optional:
-                if arg.component in fromArch:
-                    newCall(bindSym("some"), fromVar.read(fromArch, arg))
-                else:
-                    newCall(nnkBracketExpr.newTree(bindSym("none"), arg.type))
-        result.add quote do:
-            `output`[`i`] = `value`
+    when isFastCompileMode():
+        return newStmtList()
+    else:
+        result = newStmtList()
+        for i, arg in directive.args:
+            let value = case arg.kind
+                of DirectiveArgKind.Exclude:
+                    newCall(nnkBracketExpr.newTree(bindSym("Not"), arg.type), newLit(0'i8))
+                of DirectiveArgKind.Include:
+                    fromVar.read(fromArch, arg)
+                of DirectiveArgKind.Optional:
+                    if arg.component in fromArch:
+                        newCall(bindSym("some"), fromVar.read(fromArch, arg))
+                    else:
+                        newCall(nnkBracketExpr.newTree(bindSym("none"), arg.type))
+            result.add quote do:
+                `output`[`i`] = `value`
 
-proc buildConverterProc(ctx: GenerateContext, convert: ConverterDef): NimNode =
+proc buildConverterProc(ctx: GenerateContext, convert: ConverterDef): NimNode {.used.} =
     ## Builds a single converter proc
     let name = ctx.converterName(convert)
     let inputTuple = convert.input.asStorageTuple
