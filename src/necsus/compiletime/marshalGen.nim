92a990747b015ade1198a76045cfd94975798cfe
Nycto <James@RoundEights.com>
2024-06-19 08:49:02 -0700

    Disable save/restore in fast compile mode

--- src/necsus/compiletime/marshalGen.nim
+++ src/necsus/compiletime/marshalGen.nim
@@ -1,12 +1,12 @@
 import macros, codeGenInfo, common, parse, tickGen, tools, std/[sequtils, tables, json, jsonutils, sets]
 
-proc saveTypeName(genInfo: CodeGenInfo): NimNode = ident(genInfo.app.name & "Marshal")
+proc saveTypeName(genInfo: CodeGenInfo): NimNode {.used.} = ident(genInfo.app.name & "Marshal")
 
-proc restoreSysType(sys: ParsedSystem): NimNode =
+proc restoreSysType(sys: ParsedSystem): NimNode {.used.} =
     ## Returns the type that a restoreSys accepts for restoration
     sys.prefixArgs[0][1]
 
-proc collectMarshalTypes(topics: openArray[NimNode], dedupe: var HashSet[string], records: var NimNode) =
+proc collectMarshalTypes(topics: openArray[NimNode], dedupe: var HashSet[string], records: var NimNode) {.used.} =
     var seen = initTable[string, NimNode]()
     for topicType in topics:
         let topicName = topicType.strVal
@@ -24,68 +24,82 @@ proc collectMarshalTypes(topics: openArray[NimNode], dedupe: var HashSet[string]
 
 proc createSaveType(genInfo: CodeGenInfo): NimNode =
     ## Generates the type definition needed to serialize an app
-    var records = nnkRecList.newTree()
-    var dedupe = initHashSet[string]()
+    when isFastCompileMode():
+        return newEmptyNode()
+    else:
+        var records = nnkRecList.newTree()
+        var dedupe = initHashSet[string]()
 
-    genInfo.systems.filterIt(it.phase == SaveCallback).mapIt(it.returns).collectMarshalTypes(dedupe, records)
-    genInfo.systems.filterIt(it.phase == RestoreCallback).mapIt(it.restoreSysType).collectMarshalTypes(dedupe, records)
+        genInfo.systems.filterIt(it.phase == SaveCallback).mapIt(it.returns).collectMarshalTypes(dedupe, records)
+        genInfo.systems.filterIt(it.phase == RestoreCallback).mapIt(it.restoreSysType).collectMarshalTypes(dedupe, records)
 
-    return nnkTypeSection.newTree(
-        nnkTypeDef.newTree(
-            genInfo.saveTypeName,
-            newEmptyNode(),
-            nnkObjectTy.newTree(newEmptyNode(), newEmptyNode(), records)
+        return nnkTypeSection.newTree(
+            nnkTypeDef.newTree(
+                genInfo.saveTypeName,
+                newEmptyNode(),
+                nnkObjectTy.newTree(newEmptyNode(), newEmptyNode(), records)
+            )
         )
-    )
 
 let streamIdent {.compileTime.} = "stream".ident
-let decoded {.compileTime.} = "decoded".ident
+let decoded {.compileTime, used.} = "decoded".ident
 
 proc createRestoreProc(genInfo: CodeGenInfo): NimNode =
     ## Generates a proc that is able to restore all procs
     let appStateType = genInfo.appStateTypeName
 
-    let saveTypeName = genInfo.saveTypeName
+    let body = when isFastCompileMode():
+        newStmtList()
+    else:
+        let saveTypeName = genInfo.saveTypeName
 
-    var invocations = newStmtList()
-    for restore in genInfo.systems.filterIt(it.phase == RestoreCallback):
-        let readProp = newDotExpr(decoded, restore.restoreSysType.strVal.ident)
-        invocations.add(genInfo.invokeSystem(restore, {RestoreCallback}, [ readProp ]))
+        var invocations = newStmtList()
+        for restore in genInfo.systems.filterIt(it.phase == RestoreCallback):
+            let readProp = newDotExpr(decoded, restore.restoreSysType.strVal.ident)
+            invocations.add(genInfo.invokeSystem(restore, {RestoreCallback}, [ readProp ]))
 
-    let log = emitSaveTrace("Restoring from ", streamIdent, " as ", decoded)
+        let log = emitSaveTrace("Restoring from ", streamIdent, " as ", decoded)
+        quote:
+            var `decoded`: `saveTypeName`
+            fromJson(`decoded`, parseJson(`streamIdent`))
+            `log`
+            `invocations`
 
     return quote:
         proc restore*(
             `appStateIdent`: var `appStateType`,
             `streamIdent`: string
         ) {.gcsafe, raises: [IOError, OSError, JsonParsingError, ValueError, Exception].} =
-            var `decoded`: `saveTypeName`
-            fromJson(`decoded`, parseJson(`streamIdent`))
-            `log`
-            `invocations`
+            `body`
 
 proc createSaveProc(genInfo: CodeGenInfo): NimNode =
     ## Generates a proc that calls all the 'save' systems and aggregates them into a single value
     let appStateType = genInfo.appStateTypeName
 
-    var construct = nnkObjConstr.newTree(genInfo.saveTypeName)
-    for system in genInfo.systems.filterIt(it.phase == SaveCallback):
-        construct.add(
-            nnkExprColonExpr.newTree(
-                system.returns.strVal.ident,
-                genInfo.invokeSystem(system, {SaveCallback})
+    let body = when isFastCompileMode():
+        newStmtList()
+    else:
+        var construct = nnkObjConstr.newTree(genInfo.saveTypeName)
+        for system in genInfo.systems.filterIt(it.phase == SaveCallback):
+            construct.add(
+                nnkExprColonExpr.newTree(
+                    system.returns.strVal.ident,
+                    genInfo.invokeSystem(system, {SaveCallback})
+                )
             )
-        )
 
-    let log = emitSaveTrace("Saved: ", ident("result"))
+        let log = emitSaveTrace("Saved: ", ident("result"))
+
+        quote:
+            result = $toJson(`construct`)
+            `log`
 
     return quote:
         {.hint[XCannotRaiseY]:off.}
         proc save*(
             `appStateIdent`: var `appStateType`,
         ): string {.raises: [IOError, OSError, ValueError, Exception].} =
-            result = $toJson(`construct`)
-            `log`
+            `body`
 
 proc createMarshalProcs*(genInfo: CodeGenInfo): NimNode =
     ## Generates procs needed for saving and restoring game state
