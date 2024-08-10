741a4d0a1793d59dbe797e823490cd5497bd425a
Nycto <James@RoundEights.com>
2024-08-10 16:33:11 -0700

    Resolve mailbox name collisions

--- src/necsus/compiletime/parse.nim
+++ src/necsus/compiletime/parse.nim
@@ -510,7 +510,7 @@ proc instancedInfo*(system: ParsedSystem): Option[tuple[fieldName: NimNode, typ:
 
 proc callbackSysMailboxName*(system: ParsedSystem): NimNode =
     ## The name of the mailbox to use for an event callback system
-    ident("event_mailbox_" & system.symbol.strVal)
+    ident("event_mailbox_" & system.symbol.signatureHash)
 
 proc callbackSysType*(system: ParsedSystem): NimNode =
     ## Returns the event type handled by a callback system
