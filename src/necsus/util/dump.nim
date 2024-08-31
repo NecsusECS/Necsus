af38bc565562bdd094bbe106a8d032bcc6958b78
Nycto <James@RoundEights.com>
2024-08-30 18:41:41 -0700

    Fix variable names in dump

--- src/necsus/util/dump.nim
+++ src/necsus/util/dump.nim
@@ -1,4 +1,4 @@
-import std/[macros, strutils, sets]
+import std/[macros, strutils, sets, tables]
 import ../compiletime/[parse, systemGen]
 
 proc modulePath(node: NimNode): string =
@@ -65,6 +65,40 @@ proc dumpImports(app: ParsedApp, systems: openarray[ParsedSystem]) =
         if moduleName != "":
             echo "import ", moduleName, " {.all.}"
 
+proc fixVarNames(node: NimNode, symbols: var TableRef[string, NimNode]): NimNode =
+    ## Replaces any variable names that start with ':' with a copy/pastable name
+    if node.kind == nnkSym and node.strVal.startsWith(':'):
+        return symbols.mgetOrPut(node.strVal, genSym(nskLet, "tempValue"))
+    elif node.len > 0:
+        result = node.kind.newTree()
+        for child in node:
+            result.add(child.fixVarNames(symbols))
+    else:
+        return node
+
+proc fixTempVars(output: NimNode): NimNode =
+    ##
+    ## Fixes situations where Nim produces invalid code, like this:
+    ##
+    ## appState.config =
+    ##   let :tmp = 10000
+    ##   newNecsusConf(:tmp, ceilDiv(:tmp, 3))
+    ##
+    case output.kind
+    of nnkAsgn:
+        if output[1].kind != nnkStmtListExpr:
+            return output
+        var repaired = newStmtList()
+        repaired.add(output[1][0..<1])
+        repaired.add(nnkAsgn.newTree(output[0], output[1][^1]))
+        var symbols = newTable[string, NimNode]()
+        return repaired.fixVarNames(symbols)
+    of nnkStmtList, nnkProcDef:
+        result = output.kind.newTree
+        for child in output: result.add(child.fixTempVars())
+    else:
+        return output
+
 proc dumpGeneratedCode*(output: NimNode, app: ParsedApp, systems: openarray[ParsedSystem]) =
     ## Prints the generated necsus app for debugging purposes
     echo "import std/[math, json, jsonutils, options, importutils]"
@@ -73,7 +107,7 @@ proc dumpGeneratedCode*(output: NimNode, app: ParsedApp, systems: openarray[Pars
 
     echo "const DEFAULT_ENTITY_COUNT = 1_000"
     var line: string
-    for rawLine in output.repr.splitLines():
+    for rawLine in output.fixTempVars().repr.splitLines():
         line &= rawLine
             .replace("proc =destroy", "proc `=destroy`")
             .replace("proc =copy", "proc `=copy`")
