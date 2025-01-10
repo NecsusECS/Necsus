19bc570778efa032d5aaac66d31f8efa79012f21
Nycto <James@RoundEights.com>
2025-01-09 18:31:15 -0800

    Add generations to entity IDs

--- src/necsus/runtime/entityId.nim
+++ src/necsus/runtime/entityId.nim
@@ -1,19 +1,36 @@
-import hashes
+import std/[hashes, bitops, strformat]
 
 type
     EntityId* = distinct uint
         ## Identity of an entity
 
-proc `$`*(entityId: EntityId): string =
-    ## Stringify an EntityId
-    "EntityId(" & $int(entityId) & ")"
+const GENERATION_BITS = 16
+
+const ID_BITS = sizeof(EntityId) * 8 - GENERATION_BITS
+
+const GENERATION_MASK = high(EntityId).uint shl ID_BITS
+
+const ID_MASK = high(EntityId).uint shr GENERATION_BITS
 
 proc `==`*(a, b: EntityId): bool =
     ## Compare two entities
     a.uint == b.uint
 
-proc hash*(entityId: EntityId): Hash =
-    Hash(entityId.uint * 7)
+proc toInt*(entityId: EntityId): uint {.inline.} =
+    bitand(uint(entityId), ID_MASK)
 
-proc toInt*(entityId: EntityId): uint =
-    entityId.uint
+proc hash*(entityId: EntityId): Hash =
+    Hash(entityId.toInt * 7)
+
+proc generation(entityId: EntityId): uint {.inline.} =
+    ## Returns the current generation of this entity
+    bitand(uint(entityId), GENERATION_MASK).shr(ID_BITS)
+
+proc incGen*(entityId: EntityId): EntityId =
+    ## Increments the generation of an entity
+    let newgen = (entityId.generation + 1).shl(ID_BITS)
+    return EntityId(bitor(newgen, entityId.toInt))
+
+proc `$`*(entityId: EntityId): string =
+    ## Stringify an EntityId
+    fmt"EntityId({entityId.generation}:{entityId.toInt})"
