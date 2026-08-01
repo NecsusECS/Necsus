a33ea2d06977f62103146596e65398a4fb0792a4
Nycto <James@RoundEights.com>
2026-07-31 19:32:27 -0700

    Keep bookkeeping out of block store rows

--- src/necsus/util/blockstore.nim
+++ src/necsus/util/blockstore.nim
@@ -1,28 +1,44 @@
 import std/[deques, options]
 
+const bitsPerWord = 64
+
 type
-  EntryData[V] = object
-    idx: uint
-    alive: bool
-    value: V
-
-  Entry*[V] = ptr EntryData[V]
-
-  BlockStore*[V] = ref object ## Stores a block of packed values
+  BlockStoreObj[V] = object
     nextId: uint
     hasRecycledValues: bool
     recycle: Deque[uint]
-    data: seq[EntryData[V]]
+    data: seq[V]
+    liveness: seq[uint64]
     len: Natural
+
+  BlockStore*[V] = ref BlockStoreObj[V] ## Stores a block of packed values
+
+  Entry*[V] = object
+    ## A reserved slot in a `BlockStore`. Liveness is tracked in a bitmap alongside the
+    ## values instead of inline, which keeps the stored rows free of bookkeeping
+    store: ptr BlockStoreObj[V]
+    idx: uint
 
   BlockIter* = object
     index: uint
     isDone: bool
 
+template wordOf(idx: uint): int =
+  int(idx div bitsPerWord)
+
+template bitOf(idx: uint): uint64 =
+  1'u64 shl (idx mod bitsPerWord)
+
+proc wordsFor(size: Natural): Natural =
+  ## The number of bitmap words needed to track `size` values
+  Natural((size + bitsPerWord - 1) div bitsPerWord)
+
 proc newBlockStore*[V](size: Natural): BlockStore[V] =
   ## Instantiates a new BlockStore
   BlockStore[V](
-    recycle: initDeque[uint](size.int div 2), data: newSeq[EntryData[V]](size)
+    recycle: initDeque[uint](size.int div 2),
+    data: newSeq[V](size),
+    liveness: newSeq[uint64](size.wordsFor),
   )
 
 proc isFirst*(iter: BlockIter): bool =
@@ -34,6 +50,10 @@ proc isDone*(iter: BlockIter): bool {.inline.} =
 func len*[V](blockstore: var BlockStore[V]): Natural =
   ## Returns the length of this blockstore
   blockstore.len
+
+proc isAlive[V](store: BlockStoreObj[V], idx: uint): bool {.inline.} =
+  ## Whether the value at an index is live
+  (store.liveness[idx.wordOf] and idx.bitOf) != 0
 
 proc reserve*[V](blockstore: var BlockStore[V]): Entry[V] =
   ## Reserves a slot for a value
@@ -52,18 +72,18 @@ proc reserve*[V](blockstore: var BlockStore[V]): Entry[V] =
     raise newException(IndexDefect, "Storage capacity exceeded: " & $index)
 
   blockstore.len += 1
-  result = addr blockstore.data[index]
-  result.idx = index
+  return Entry[V](store: addr blockstore[], idx: index)
 
 proc index*[V](entry: Entry[V]): uint {.inline.} = ## Returns the index of an entry
   entry.idx
 
 template value*[V](entry: Entry[V]): var V = ## Returns the value of an entry
-  entry.value
+  entry.store.data[entry.idx]
 
 proc commit*[V](entry: Entry[V]) {.inline.} =
   ## Marks that an entry is ready to be used
-  entry.alive = true
+  entry.store.liveness[entry.idx.wordOf] =
+    entry.store.liveness[entry.idx.wordOf] or entry.idx.bitOf
 
 template set*[V](entry: Entry[V], newValue: V) =
   ## Sets a value on an entry
@@ -78,21 +98,22 @@ template push*[V](store: var BlockStore[V], newValue: V): uint =
 
 proc del*[V](store: var BlockStore[V], idx: uint): V =
   ## Deletes a field
-  if store.data[idx].alive:
-    store.data[idx].alive = false
+  if unlikely(idx >= store.data.len.uint):
+    raise newException(IndexDefect, "index out of bounds: " & $idx)
+  if store[].isAlive(idx):
+    store.liveness[idx.wordOf] = store.liveness[idx.wordOf] and not idx.bitOf
     store.len -= 1
-    let deleted = move(store.data[idx])
-    result = deleted.value
+    result = move(store.data[idx])
     store.recycle.addLast(idx)
     store.hasRecycledValues = true
 
 proc `[]`*[V](store: BlockStore[V], idx: uint): var V =
   ## Reads a field
-  store.data[idx].value
+  store.data[idx]
 
 template `[]=`*[V](store: BlockStore[V], idx: uint, newValue: V) =
   ## Sets a new value for a key
-  store.data[idx].value = newValue
+  store.data[idx] = newValue
 
 proc next*[V](store: var BlockStore[V], iter: var BlockIter): ptr V {.inline.} =
   ## Returns the next value in an iterator
@@ -100,11 +121,10 @@ proc next*[V](store: var BlockStore[V], iter: var BlockIter): ptr V {.inline.} =
     if unlikely(store == nil or iter.index >= store.nextId):
       iter.isDone = true
       return nil
-    elif likely(store.data[iter.index].alive):
-      iter.index += 1
-      return addr store.data[iter.index - 1].value
-    else:
-      iter.index += 1
+    let idx = iter.index
+    iter.index = idx + 1
+    if likely(store[].isAlive(idx)):
+      return addr store.data[idx]
 
 iterator items*[V](store: var BlockStore[V]): var V =
   ## Iterate through all values in this BlockStore
