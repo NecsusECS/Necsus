877fb8ac7b6e12234e5e387502a8e4680cb2727c
Nycto <James@RoundEights.com>
2026-07-31 20:18:18 -0700

    Add spawnDuringIteration test

---
+++ tests/t_spawnDuringIteration.nim
@@ -0,0 +1,27 @@
+import unittest, necsus, sequtils, algorithm
+
+type Thingy = object
+  number: int
+
+proc setup(spawn: Spawn[(Thingy,)]) =
+  for i in 1 .. 3:
+    spawn.with(Thingy(number: i))
+
+proc grow(all: Query[(Thingy,)], spawn: Spawn[(Thingy,)]) =
+  var visited: seq[int]
+  for comp in all:
+    visited.add(comp[0].number)
+    if comp[0].number < 3:
+      spawn.with(Thingy(number: comp[0].number * 100))
+  check(visited == @[1, 2, 3])
+
+proc assertions(all: Query[(Thingy,)]) =
+  check(toSeq(all.items).mapIt(it[0].number).sorted == @[1, 2, 3, 100, 200])
+
+proc runner(tick: proc(): void) =
+  tick()
+
+proc myApp() {.necsus(runner, [~setup, ~grow, ~assertions], newNecsusConf()).}
+
+test "Entities spawned during iteration are skipped":
+  myApp()
